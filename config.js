a17cf50fa8c588bcbae87aebe06824aff3b6e83a
Hasibul Hasan <hasibulfor@gmail.com>
2019-11-29 06:10:30 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    url: 'mongodb+srv://hasibulfor:3RDp@r8!@cluster0-btcf6.mongodb.net/test?retryWrites=true&w=majority',
+    url: 'mongodb+srv://hasibulfor:2NDp@p3!@cluster0-btcf6.mongodb.net/test?retryWrites=true&w=majority',
     serverport: 3000 
 }
